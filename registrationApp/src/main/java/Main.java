3a450db20b136e1a7ffecde153f8fe3cb46c6cbb
Ашихмин Игорь <imamiracle888@gmail.com>
2021-10-03 22:48:47 +0700

    should be final version

--- registrationApp/src/main/java/Main.java
+++ registrationApp/src/main/java/Main.java
@@ -7,10 +7,8 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args){
-        //new JFormattedTextFieldTest();
         File usersData = new File("src\\main\\resources", "usersData.txt");
         JFrame frame = new JFrame("app");
-        GridBagLayout layout = new GridBagLayout();
         RegistrationFrame registrationFrame = new RegistrationFrame(frame,usersData);
         registrationFrame.start();
     }
